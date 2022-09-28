1e8ad4ae9a6b0a32a20f1bfc6b92e01b5601a036
Vivi Baute <vivscodes@hotmail.com>
2022-09-28 20:47:09 +0100

    Cambios finales

--- AUT02_02_BauteViviana_Listas/Controllers/PersonajeController.cs
+++ AUT02_02_BauteViviana_Listas/Controllers/PersonajeController.cs
@@ -5,19 +5,14 @@ namespace AUT02_02_BauteViviana_Listas.Controllers
 {
     public class PersonajeController : Controller
     {
-        public static List<Personaje> myList = new List<Personaje>();
-        public void LPersonajes()
+        public static List<Personaje> myList = new List<Personaje>()
         {
-            myList.Add(new Personaje { Id = 1, Name = "Phil", Family = "Dunphy", NChildren = 3 });
-            myList.Add(new Personaje { Id = 2, Name = "Gloria Delgado", Family = "Pritchett", NChildren = 1 });
+            new Personaje { Id = 1, Name = "Phil", Family = "Dunphy", NChildren = 3 },
+            new Personaje { Id = 2, Name = "Gloria Delgado", Family = "Pritchett", NChildren = 1 }
+        };
 
-        }
         public IActionResult Index()
         {
-            if (myList.Count.Equals(0))
-            {
-                LPersonajes();
-            }
             return View(myList);
         }
         public IActionResult Create()
@@ -25,16 +20,16 @@ namespace AUT02_02_BauteViviana_Listas.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Personaje newPersonaje)
+        public IActionResult Create(int id, string Name, string Family, int NChildren)
         {
             if (ModelState.IsValid)
             {
-                myList.Add(newPersonaje);
+                myList.Add(new Personaje { Id = myList.Count + 1, Name = Name, Family = Family, NChildren = NChildren });
                 return RedirectToAction("Index");
             }
             else
             {
-                return View(newPersonaje);
+                return View();
             }
         }
 
